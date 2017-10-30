0d06dbdddaab2637b58cdcb1bb8be190496362a9
JONATHAN CRUZ ROCHA FERREIRA <jonathan.ferreira@animaeducacao.com.br>
2017-10-30 15:22:24 -0200

    adding action on buttons

--- index.vr.js
+++ index.vr.js
@@ -2,30 +2,106 @@ import React from 'react';
 import {
   AppRegistry,
   asset,
+  StyleSheet,  
   Pano,
   Text,
   View,
+  Model,
+  AmbientLight,
 } from 'react-vr';
 
+import Button from './vr/button.js';
+
 export default class EarthMoonVR extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      rotation: 130,
+      zoom: -70,
+    };
+    this.lastUpdate = Date.now();
+
+    this.spaceSkymap = [
+      '../static_assets/space_right1.png',
+      '../static_assets/space_left2.png',
+      '../static_assets/space_top3.png',
+      '../static_assets/space_bottom4.png',
+      '../static_assets/space_front5.png',
+      '../static_assets/space_back6.png'
+    ];
+
+    this.rotate = this.rotate.bind(this);
+
+    this.styles = StyleSheet.create({
+      menu: {
+        flex: 1,
+        flexDirection: 'column',
+        width: 1,
+        alignItems: 'stretch',
+        transform: [{ translate: [2, 2, -5] }]
+      }
+    })
+  }
+  comonentDidMount() {
+    this.rotate()
+  }
+  componentWilUnmont() {
+    if(this.frameHandle) {
+      cancelAnimationFrame(this.frameHandle)
+      this.frameHandle = null;
+    }
+  }
+  rotate() {
+    const now = Date.now()
+    const delta = now - this.lastUpdate;
+    this.lastUpdate = now;
+    this.setState({
+      rotation: this.state.rotation + delta / 150
+    })
+    this.frameHandle = requestAnimationFrame(this.rotate);
+  }
   render() {
     return (
       <View>
-        <Pano source={
-          {
-            uri: [
-              './static_assets/space_right1.png',
-              './static_assets/space_left2.png',
-              './static_assets/space_top3.png',
-              './static_assets/space_bottom4.png',
-              './static_assets/space_back6.pngx',
-              './static_assets/space_front5.png'
-            ]
-          }
-        }
+        <Pano source={ { uri: this.spaceSkymap } }/>
+
+        <AmbientLight intensity={ 2.6 }  />
+
+        <View style={ this.styles.menu }>
+          <Button text='+'
+            callback={()=> this.setState((prevState) => ({ zoom: prevState.zoom + 10 }) ) } />
+          <Button text='-'
+            callback={()=> this.setState((prevState) => ({ zoom: prevState.zoom - 10 }) ) } />
+        </View>
+        
+        <Model
+          style={{
+            transform: [
+              {translate: [-25, 0, this.state.zoom]},
+              {scale: 0.05 },
+              {rotateY: this.state.rotation},
+              {rotateX: 20},
+              {rotateZ: -10}
+            ],
+          }}
+          source={{obj:asset('earth.obj'), mtl:asset('earth.mtl')}}
+          lit={true}
         />
+        
+        <Model
+          style={{
+            transform: [
+              {translate: [10, 10, this.state.zoom - 30]},
+              {scale: 0.05},
+              {rotateY: this.state.rotation / 3},
+            ],
+          }}
+          source={{obj:asset('moon.obj'), mtl:asset('moon.mtl')}}
+          lit={true}
+        />
+  
       </View>
-    );
+  );
   }
 };
 
